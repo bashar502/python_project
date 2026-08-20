0a456d6edfd2ce280e563d1e50ed8d697847bce1
bashar502 <mrkhanbasar07@gmail.com>
2026-08-20 11:58:21 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,36 @@
+# print("hello world")
+# print("you are a good person")
+# print('''you're a"good"person''')
+# print("python is fun")
+# print('''"quotes"and'single quotes'can be tricky''')
+# print("\"quotes\"and'single quotes'can be tricky.")
+
+
+# print("Python is fun.\n\"Quotes\" and 'single quotes' can be tricky.")
+
+# variable_name = value
+
+# welcome = "Hello World"
+
+# print(welcome)
+
+
+# variable = "how are you"
+
+# print(variable)
+
+
+# name = "bashar"
+# age = "20"
+# city = "bhubneswar"
+# print(name,age,city)
+
+name = "bashar"
+age = 20
+city = "bhubneswar"
+print(name,age,city)
+
+print(f"My name is {name} from {city} & I'm {age}")
+
+
+
